ef494ae9d27e98b6f6d528ca0150be8a554c3c0f
x1AoCyAn <83789289@qq.com>
2020-07-06 22:26:01 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -5,12 +5,8 @@ import requests,json,os
 # server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
 sever = 'on'
 # 填写server酱sckey,不开启server酱则不用填
-sckey = os.environ["SCKEY"]
-#'SCU104607T640b5c34020361870ddf16d2f813df265f0329a22511c'
-# 填入glados账号对应cookie
-cookie = os.environ["COOKIE"]
-#'__cfduid=d759102f672690cf8527fea8014437b5c1593932142; _ga=GA1.2.1312965143.1593932149; _gid=GA1    .2.848935626.1594042977; koa:sess=eyJ1c2VySWQiOjQyNjgzLCJfZXhwaXJlIjoxNjE5OTYyOTkwNDA4LCJfbWF4QWdlIjoyNTk    yMDAwMDAwMH0=; koa:sess.sig=beDUK17Rp_VjRKYkIsQ0SfhUqfE'
-
+sckey = 'SCU104607T640b5c34020361870ddf16d2f813df265f0329a22511c'
+cookie = '__cfduid=d759102f672690cf8527fea8014437b5c1593932142; _ga=GA1.2.1312965143.1593932149; _gid=GA1.2.848935626.1594042977; koa:sess=eyJ1c2VySWQiOjQyNjgzLCJfZXhwaXJlIjoxNjE5OTYyOTkwNDA4LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=beDUK17Rp_VjRKYkIsQ0SfhUqfE'
 
 
 def start():
