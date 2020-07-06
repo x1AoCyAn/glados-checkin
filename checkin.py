221b6b5ab28dab1154b59026bac935d16f4a0a0a
x1AoCyAn <83789289@qq.com>
2020-07-06 22:24:04 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -3,7 +3,7 @@
 import requests,json,os
 
 # server酱开关，填off不开启(默认)，填on同时开启cookie失效通知和签到成功通知
-sever = os.environ["on"]
+sever = 'on'
 # 填写server酱sckey,不开启server酱则不用填
 sckey = os.environ["SCKEY"]
 #'SCU104607T640b5c34020361870ddf16d2f813df265f0329a22511c'
